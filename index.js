a5c11058d921dd367002264f1e61caa111d26876
isabelleadrury <isabelledrury38@gmail.com>
2025-08-19 13:39:30 -0400

    added average rate and array functions

--- index.js
+++ index.js
@@ -6,7 +6,33 @@
  */
 
 // === Constants ===
-const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
+const NAMES = ["Bob", "Alice", "Carol", "Eve", "Dave"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
-const PRICE_RANGE = { min: 20, max: 200 };
-const NUM_FREELANCERS = 100;
+const PRICE_RANGE = [115, 64, 188, 118, 159];
+const NUM_FREELANCERS = [100];
+
+const NUM_PEOPLE = NAMES.length;
+
+const freelancers = Array.from({ length: NUM_PEOPLE }, makeFreelancers);
+
+const averageRate = getAverageRate();
+
+console.log(averageRate, freelancers);
+
+function makeFreelancers(_, index) {
+  return {
+    name: NAMES[index],
+    occupation: OCCUPATIONS[index],
+    range: PRICE_RANGE[index],
+  };
+}
+
+function getAverageRate() {
+  const total =
+    freelancers.reduce((accum, currentFreelancers) => {
+      return accum + currentFreelancers.range;
+    }, 0) / NUM_PEOPLE;
+  return total;
+}
+
+console.log(averageRate);
